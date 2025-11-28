3faf6de1c411d199bd4768e5fcac5e026706f0d7
Mohamed Elgazar <mohamedelgazar498@gmail.com>
2025-11-28 08:42:12 +0200

    cleaned structure

--- libs/platform/src/index.ts
+++ libs/platform/src/index.ts
@@ -1,7 +1,2 @@
 export * from './interface/ports/repo-fetch.port';
 export * from './interface/ports/workdir.port';
-
-// 🔴 INTENTIONAL ERRORS FOR CI TESTING 🔴
-var unusedVariable = "This will trigger lint errors!";  // Using 'var' instead of 'const' + unused variable
-const x = 1;  // Unused variable
-
