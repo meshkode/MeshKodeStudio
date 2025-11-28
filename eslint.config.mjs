980ce238fd28141351eb698f6f8bc7e2c6c9baeb
Mohamed Elgazar <mohamedelgazar498@gmail.com>
2025-11-28 08:25:16 +0200

    fourth test try

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', '**/workflow-bundle.js'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
